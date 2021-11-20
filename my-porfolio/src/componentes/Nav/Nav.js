1344759203082d8f0e9a75f888cf42da08b5daff
Nestor Raul Camacho <nestrra@gmail.com>
2021-11-20 17:46:54 -0500

    estiloss

--- my-porfolio/src/componentes/Nav/Nav.js
+++ my-porfolio/src/componentes/Nav/Nav.js
@@ -4,28 +4,27 @@ import './Nav.css'
 export const Nav = () => {
     return (
         <div className="nav-container">
-               
-               <a href=""><div className="logo">
-                    
-                </div></a>
-                
 
-                            <div>
-                    <ul>
-                        <li>
-                           <a href="">Inicio</a>
-                        </li>
-                        <li>
+            <div className="logo">
+                <a href=""></a>
+            </div>
+
+            <div className="ul">
+                <ul>
+                    <li>
+                        <a href="">Inicio</a>
+                    </li>
+                    <li>
                         <a href="#about">Sobre mi</a>
-                        </li>
-                        <li>
+                    </li>
+                    <li>
                         <a href="#proyectos">Experiencia</a>
-                        </li>
-                        <li>
+                    </li>
+                    <li>
                         <a href="#footer">Contacto</a>
-                        </li>
-                    </ul>
-                </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
