f351195ba299113521e033be61961462781485a5
Nestor Raul Camacho <nestrra@gmail.com>
2021-11-28 22:39:43 -0500

    cambios_estilos_

--- my-porfolio/src/componentes/HomeScreen.js
+++ my-porfolio/src/componentes/HomeScreen.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import { Header } from './header/Header'
-import { About } from './About'
-import { Footer } from './footer/Footer'
-import { Habiliadades } from './habilidades/Habiliadades'
 
-import { Proyectos } from './Proyectos/Proyectos'
 import { Index } from './index/Index'
 
 
